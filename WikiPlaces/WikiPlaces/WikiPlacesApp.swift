cc240c6e5c45a195ef1903f8fce3ac6bf5253f0a
Salih Guc <salihguc@yandex.com>
2026-02-22 14:42:29 +0100

    init: create places app

---
+++ WikiPlaces/WikiPlaces/WikiPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiPlacesApp.swift
+//  WikiPlaces
+//
+//  Created by sg on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WikiPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WikiPlaces/WikiPlacesTests/WikiPlacesTests.swift
@@ -0,0 +1,16 @@
+//
+//  WikiPlacesTests.swift
+//  WikiPlacesTests
+//
+//  Created by sg on 22.02.2026.
+//
+
+import Testing
+
+struct WikiPlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
